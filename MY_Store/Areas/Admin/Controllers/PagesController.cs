34bad50c260423ae6fadea426a7979b1d44c6aa9
Шевчук Сергій Ігорович <serh007@gmail.com>
2020-09-20 01:13:12 +0300

    AddPage

---
+++ MY_Store/Areas/Admin/Controllers/PagesController.cs
@@ -0,0 +1,55 @@
+﻿using MY_Store.Models.Data;
+using MY_Store.Models.ViewModels.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MY_Store.Areas.Admin.Controllers
+{
+    public class PagesController : Controller
+    {
+        // GET: Admin/Pages
+        public ActionResult Index()
+        {
+            List<PageVM> pageList;
+            using (Db db = new Db())
+            {
+                pageList = db.Pages.ToArray().OrderBy(x => x.Sorting).Select(x => new PageVM(x)).ToList();
+            }
+
+            return View(pageList);
+        }
+
+        // GET: Admin/Pages/AddPage
+        [HttpPost]
+        public ActionResult AddPage()
+        {           
+            return View();
+        }
+
+        // POST: Admin/Pages/AddPage
+        [HttpPost]
+        public ActionResult AddPage(PageVM model)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            using (Db db = new Db())
+            {
+                string slug;
+                PagesDTO dto = new PagesDTO();
+
+                dto.Title = model.Title.ToUpper();
+
+
+
+                //plan
+            }
+
+        }
+    }
+}
---
+++ MY_Store/Models/Data/Db.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MY_Store.Models.Data
+{
+    public class Db : DbContext
+    {
+        public DbSet<PagesDTO> Pages { get; set; }
+    }
+}
